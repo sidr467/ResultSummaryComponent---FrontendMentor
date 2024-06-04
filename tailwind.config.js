86ef5e129e96f6ffa8a005cd75427462dff62b68
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-06-04 14:41:02 +0530

    Custom styles in tailwind config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/*.{html,js}"],
+  content: ["*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      // ### Primary
+      PrimaryLightRed: "hsl(0, 100%, 67%)",
+      PrimaryOrangeyYellow: " hsl(39, 100%, 56%)",
+      PrimaryGreenTeal: "hsl(166, 100%, 37%)",
+      PrimaryCobaltblue: "hsl(234, 85%, 45%)",
+      // ## Gradients
+      BgLightslateBlue: "hsl(252, 100%, 67%)",
+      BgLightroyalBlue: "hsl(241, 81%, 54%)",
+      CircleVioletBlue: "hsla(256, 72%, 46%, 1)",
+      CirclePersianBlue: "hsla(241, 72%, 46%, 0)",
+      // ### Neutral
+      White: "hsl(0, 0%, 100%)",
+      PaleBlue: "hsl(221, 100%, 96%)",
+      LightLavender: "hsl(241, 100%, 89%)",
+      DarkgrayBlue: "hsl(224, 30%, 27%)",
+    },
+    fontSize:{
+      ParaFont : "18px",
+    },
+    fontWeight:{
+      Fontlight: "500",
+      Fontmedium: "700",
+      Fontbold :"800"
+    }
   },
   plugins: [],
 }
